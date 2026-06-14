78d18e603af7c1011f5542dd900ecb5db18988ca
leandro-bsf <leandrobs1710@gmail.com>
2026-06-14 11:31:13 -0300

    ajuste vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Adicione isso se você quiser o novo modo do Vite
 
 export default defineConfig({
-  plugins: [
-    react(),
-  
-  ],
+  plugins: [react()],
 })
